2655a1e0c91d733965e517082d3d7ad45e3ac403
-Ry <ryanpayne0511@gmail.com>
2026-06-14 21:55:35 +0100

    test/mock impl for the sdk generator using the PyModule class

--- WillowGen/WillowSdkGenerator.cs
+++ WillowGen/WillowSdkGenerator.cs
@@ -1,5 +1,6 @@
 ﻿using UE3StubGenCore.Export;
 using UE3StubGenCore.Render;
+using WillowGen.py;
 
 namespace WillowGen;
 
@@ -7,5 +8,11 @@ public class WillowSdkGenerator : IExporter
 {
     public void Export(ExportModel model, ExportPackage pkg)
     {
+        PyModule module = new(pkg);
+        Console.WriteLine($"Module={module.Name}");
+        Console.WriteLine($"  Class Count {module.Classes.Count}");
+        Console.WriteLine($"  Struct Count {module.Structures.Count}");
+        Console.WriteLine($"  Enum Count {module.Enums.Count}");
+        Console.WriteLine("End");
     }
 }
--- WillowGen/py/PyModule.cs
+++ WillowGen/py/PyModule.cs
@@ -4,12 +4,14 @@ namespace WillowGen.py;
 
 public class PyModule : PyBaseElement
 {
+    public string Name { get; } // TODO: Remove
     public List<PyClassDef> Classes { get; } = [];
     public List<PyStructDef> Structures { get; } = [];
     public List<PyEnumDef> Enums { get; } = [];
 
     public PyModule(ExportPackage export)
     {
+        Name = export.PackageName;
         foreach (var cls in export.Classes)
         {
             Classes.Add(new PyClassDef(cls, this));
