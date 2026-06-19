062ff97ac664d061b19034739321a3cb6103be39
-Ry <ryanpayne0511@gmail.com>
2026-06-19 13:23:53 +0100

    marker comments for later

--- WillowGen/Renderers/PyParamRenderer.cs
+++ WillowGen/Renderers/PyParamRenderer.cs
@@ -7,6 +7,15 @@ namespace WillowGen.Renderers;
 
 public class PyParamRenderer(TypedParamDef elem) : IRenderable
 {
+    
+    // TODO: we need need some type of mechanism to register an import and another to resolve it.
+    //  That mechanism must consider cross-package imports and potentially duplicated names i.e.,
+    //    from foo import baz
+    //    from bar import baz # baz is already in use
+    //
+    // Probably want two things a top level completely unique reference and a file-scoped-reference
+    //
+    
     public void Render(Sink sink)
     {
         sink.Append(elem.Name());
