380bbf3e151e658de253328b66bf50c8459d9833
-Ry <ryanpayne0511@gmail.com>
2026-06-20 04:02:37 +0100

    allow name to be None

--- WillowGen/Renderers/PyStubApiRenderer.cs
+++ WillowGen/Renderers/PyStubApiRenderer.cs
@@ -9,7 +9,7 @@ public class PyStubApiRenderer : IRenderable
         sink.AppendLine("from typing import TypeVar, TypeAlias");
         sink.AppendLine("from unrealsdk.unreal import UObject");
         sink.AppendLine();
-        sink.AppendLine("type name = str");
+        sink.AppendLine("type name = str | None");
         sink.AppendLine("type byte = int");
         sink.AppendLine("type UnresolvedClass = UObject | None");
         sink.AppendLine();
